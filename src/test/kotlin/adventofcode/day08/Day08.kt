73ca20138180c554d160b692ec433d0e4ff12a69
James Carman <jcarman@callibrity.com>
2023-12-23 00:28:12 -0500

    Day 8 complete.

--- src/test/kotlin/adventofcode/day08/Day08.kt
+++ src/test/kotlin/adventofcode/day08/Day08.kt
@@ -16,32 +16,73 @@
 
 package adventofcode.day08
 
+import adventofcode.util.Repeater
+import adventofcode.util.lcm
 import adventofcode.util.readAsString
+import adventofcode.util.removeAll
+import adventofcode.util.splitByWhitespace
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class Day08 {
     @Test
     fun example1() {
-        calculatePart1(readAsString("day08-example.txt")) shouldBe 0L
+        calculatePart1(readAsString("day08-example1.txt")) shouldBe 2L
     }
 
     @Test
     fun part1() {
-        calculatePart1(readAsString("day08.txt")) shouldBe 0L
+        calculatePart1(readAsString("day08.txt")) shouldBe 13771L
     }
 
     @Test
     fun example2() {
-        calculatePart2(readAsString("day08-example.txt")) shouldBe 0L
+        calculatePart2(readAsString("day08-example2.txt")) shouldBe 6L
     }
 
     @Test
     fun part2() {
-        calculatePart2(readAsString("day08.txt")) shouldBe 0L
+        calculatePart2(readAsString("day08.txt")) shouldBe 13129439557681L
     }
 
 
-    private fun calculatePart1(input: String): Long = 0
-    private fun calculatePart2(input: String): Long = 0
+    private fun calculatePart1(input: String): Long {
+        val instructions = parseInstructions(input)
+        val mappings = parseMappings(input)
+        return followPath(mappings, instructions, "AAA")
+    }
+
+    private fun calculatePart2(input: String): Long {
+        val instructions = parseInstructions(input)
+        val mappings = parseMappings(input)
+        val initialNodes = mappings.keys.filter { it.endsWith("A") }
+        return initialNodes
+            .map { initialNode -> followPath(mappings, instructions, initialNode) { it.endsWith("Z") } }
+            .lcm()
+    }
+
+    private fun parseInstructions(input: String) = input.substringBefore("\n\n").toCharArray().toList()
+
+    private fun parseMappings(input: String) = input.substringAfter("\n\n").lines()
+        .map { it.removeAll("= \\(", ",", "\\)") }
+        .map { it.splitByWhitespace() }
+        .associate { (key, left, right) -> key to (left to right) }
+
+    private fun followPath(
+        path: Map<String, Pair<String, String>>,
+        instructions: List<Char>,
+        initialNode: String,
+        stopPredicate: (String) -> Boolean = { it == "ZZZ" }
+    ): Long {
+        val repeater = Repeater(instructions)
+        var currentNode = initialNode
+        var count = 0L
+        do {
+            val instruction = repeater.next()
+            val (left, right) = path[currentNode]!!
+            currentNode = if (instruction == 'L') left else right
+            count++
+        } while (!stopPredicate(currentNode))
+        return count
+    }
 }
